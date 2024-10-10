6efb937e154224b413f7939cf1b5eb7390392e72
jswebschiede <schoeneburg.j@googlemail.com>
2024-10-10 18:07:09 +0200

    add: markup

--- src/components/com_roombooking/tmpl/room/default.php
+++ src/components/com_roombooking/tmpl/room/default.php
@@ -11,7 +11,9 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
+use Joomla\CMS\Router\Route;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\HTML\HTMLHelper;
 
 /** @var \Joomla\Component\Roombooking\Site\View\Room\HtmlView $this */
 
@@ -19,6 +21,8 @@ use Joomla\CMS\Language\Text;
 $wa = Factory::getApplication()->getDocument()->getWebAssetManager();
 $wa->useScript('com_roombooking.main');
 $wa->useStyle('com_roombooking.style');
+
+HTMLHelper::_('bootstrap.tooltip', '.hasTooltip');
 ?>
 
 <?php if ($this->params->get('show_page_heading')): ?>
@@ -33,18 +37,64 @@ $wa->useStyle('com_roombooking.style');
 	</div>
 <?php endif; ?>
 
-<div class="com_roombooking room-<?php echo $this->item->id; ?> room-item">
-	<div class="row">
-		<div class="col-12 mb-4">
-			<h2><?php echo $this->item->name; ?></h2>
-			<div class="details small mb-3">
-				<time datetime="<?php echo $this->item->created; ?>">
-					<?php echo Text::_('JDATE'); ?>:
-					<?php echo JDate::getInstance($this->item->created)->format('d.m.Y'); ?>
-				</time>
-				<div><?php echo Text::_('JAUTHOR'); ?>: <?php echo $this->item->author; ?></div>
+<div class="com_roombooking room-<?php echo $this->item->id; ?> room-item pb-5">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<figure class="room-image figure position-relative">
+					<?php if (!empty($this->item->image)): ?>
+						<?php echo HTMLHelper::_('image', $this->item->image, $this->item->name, ['class' => 'figure-img img-fluid']); ?>
+					<?php endif; ?>
+
+					<div class="position-absolute top-0 end-0 p-2">
+						<div class="d-flex flex-wrap gap-2">
+							<span class="badge bg-primary hasTooltip larger-badge"
+								title="<?php echo Text::_('COM_ROOMBOOKING_MAX_CAPACITY'); ?>">
+								<i class="fas fa-users me-1"></i>
+								<?php echo $this->item->capacity; ?> <?php echo Text::_('COM_ROOMBOOKING_PERSONS'); ?>
+							</span>
+							<span class="badge bg-primary hasTooltip larger-badge"
+								title="<?php echo Text::_('COM_ROOMBOOKING_ROOM_SIZE'); ?>">
+								<i class="fas fa-ruler-combined me-1"></i>
+								<?php echo $this->item->size; ?> m²
+							</span>
+							<span class="badge bg-primary hasTooltip larger-badge"
+								title="<?php echo Text::_('COM_ROOMBOOKING_ROOM_PRICE'); ?>">
+								<i class="fas fa-money-bill-wave me-1"></i>
+								<?php echo $this->item->price; ?>
+								<?php echo Text::_('COM_ROOMBOOKING_CURRENCY_PER_DAY'); ?>
+							</span>
+						</div>
+					</div>
+				</figure>
 			</div>
-			<?php echo $this->item->description; ?>
 		</div>
+		<form
+			action="<?php echo Route::_('index.php?option=com_roombooking&task=room.booking&id=' . $this->item->id); ?>"
+			method="post" class="room-booking-form">
+			<div class="row">
+				<div class="col-md-5">
+					<div class="room-sidebar">
+						<h3>Raum buchen</h3>
+						<div class="recurring-booking">
+							<div class="form-check">
+								<input type="checkbox" class="form-check-input" id="recurring-booking"
+									name="recurring-booking">
+								<label for="recurring-booking" class="form-check-label">Wiederkehrende Buchung</label>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-7">
+					<h2 class="mb-4"><?php echo $this->item->name; ?></h2>
+
+					<div class="room-description">
+						<?php echo $this->item->description; ?>
+					</div>
+				</div>
+			</div>
+
+			<?php echo HTMLHelper::_('form.token'); ?>
+		</form>
 	</div>
 </div>
