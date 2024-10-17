8a05f7f8a9d9138a824c579f95a84da77167f077
jswebschiede <schoeneburg.j@googlemail.com>
2024-10-17 22:31:30 +0200

    refactor: moved placeholders and recurrence type to helper class

--- src/administrator/components/com_roombooking/src/View/Bookings/HtmlView.php
+++ src/administrator/components/com_roombooking/src/View/Bookings/HtmlView.php
@@ -16,6 +16,7 @@ use Joomla\CMS\Toolbar\ToolbarHelper;
 use Joomla\CMS\MVC\View\GenericDataException;
 use Joomla\CMS\Toolbar\Button\DropdownButton;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\Component\Roombooking\Site\Helper\RoombookingHelper;
 
 /**
  * View class for a list of bookings.
@@ -123,31 +124,13 @@ class HtmlView extends BaseHtmlView
 			throw new GenericDataException(implode("\n", $errors), 500);
 		}
 
-		$this->translateRecurrenceTypes();
+		foreach ($this->items as &$item) {
+			$item->recurrence_type = RoombookingHelper::translateRecurrenceType($item->recurrence_type);
+		}
+
 		$this->addToolbar();
 
 		parent::display($tpl);
-	}
-
-	/**
-	 * Translate recurrence types in items
-	 *
-	 * @return void
-	 */
-	protected function translateRecurrenceTypes(): void
-	{
-		$translations = [
-			'weekly' => Text::_('COM_ROOMBOOKING_WEEKLY'),
-			'biweekly' => Text::_('COM_ROOMBOOKING_BIWEEKLY'),
-			'monthly' => Text::_('COM_ROOMBOOKING_MONTHLY'),
-			'none' => Text::_('COM_ROOMBOOKING_NONE')
-		];
-
-		foreach ($this->items as &$item) {
-			if (isset($item->recurrence_type) && array_key_exists($item->recurrence_type, $translations)) {
-				$item->recurrence_type = $translations[$item->recurrence_type];
-			}
-		}
 	}
 
 	/**
--- src/components/com_roombooking/src/Helper/RoombookingHelper.php
+++ src/components/com_roombooking/src/Helper/RoombookingHelper.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * @package     com_roombooking
  * @version     1.0.0
@@ -13,6 +12,7 @@ namespace Joomla\Component\Roombooking\Site\Helper;
 
 use NumberFormatter;
 use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -46,5 +46,46 @@ abstract class RoombookingHelper
         }
 
         return $formatter->formatCurrency($price, 'EUR');
+    }
+
+    /**
+     * Translate recurrence types
+     *
+     * @param string $recurrenceType
+     * @return string
+     */
+    public static function translateRecurrenceType(string $recurrenceType): string
+    {
+        $translations = [
+            'weekly' => Text::_('COM_ROOMBOOKING_WEEKLY'),
+            'biweekly' => Text::_('COM_ROOMBOOKING_BIWEEKLY'),
+            'monthly' => Text::_('COM_ROOMBOOKING_MONTHLY'),
+            'none' => Text::_('COM_ROOMBOOKING_NONE')
+        ];
+
+        return $translations[$recurrenceType] ?? $recurrenceType;
+    }
+
+    /**
+     * Get the mail placeholders.
+     *
+     * @return array
+     */
+    public static function getMailPlaceholders(): array
+    {
+        return [
+            'booking_details' => Text::_('COM_ROOMBOOKING_MAIL_PLACEHOLDER_BOOKING_DETAILS'),
+            '{{customer_name}}' => Text::_('COM_ROOMBOOKING_MAIL_PLACEHOLDER_CUSTOMER_NAME'),
+            '{{customer_email}}' => Text::_('COM_ROOMBOOKING_MAIL_PLACEHOLDER_CUSTOMER_EMAIL'),
+            '{{customer_phone}}' => Text::_('COM_ROOMBOOKING_MAIL_PLACEHOLDER_CUSTOMER_PHONE'),
+            '{{customer_address}}' => Text::_('COM_ROOMBOOKING_MAIL_PLACEHOLDER_CUSTOMER_ADDRESS'),
+            '{{booking_date}}' => Text::_('COM_ROOMBOOKING_MAIL_PLACEHOLDER_BOOKING_DATE'),
+            '{{recurring}}' => Text::_('COM_ROOMBOOKING_MAIL_PLACEHOLDER_RECURRING'),
+            '{{recurrence_type}}' => Text::_('COM_ROOMBOOKING_MAIL_PLACEHOLDER_RECURRENCE_TYPE'),
+            '{{recurrence_end_date}}' => Text::_('COM_ROOMBOOKING_MAIL_PLACEHOLDER_RECURRENCE_END_DATE'),
+            '{{total_amount}}' => Text::_('COM_ROOMBOOKING_MAIL_PLACEHOLDER_TOTAL_AMOUNT'),
+            '{{room_id}}' => Text::_('COM_ROOMBOOKING_MAIL_PLACEHOLDER_ROOM_ID'),
+            '{{room_name}}' => Text::_('COM_ROOMBOOKING_MAIL_PLACEHOLDER_ROOM_NAME')
+        ];
     }
 }
