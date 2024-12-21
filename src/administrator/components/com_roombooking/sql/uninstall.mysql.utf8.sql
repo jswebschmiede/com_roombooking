2864a548b6586f812c6928bb336c4f94fd541aa9
jswebschiede <schoeneburg.j@googlemail.com>
2024-12-21 14:17:48 +0100

    fix: sql tables

--- src/administrator/components/com_roombooking/sql/uninstall.mysql.utf8.sql
+++ src/administrator/components/com_roombooking/sql/uninstall.mysql.utf8.sql
@@ -1,3 +1,9 @@
-DROP TABLE IF EXISTS `#__roombooking_rooms`;
+DROP TABLE IF EXISTS `#__roombooking_booking_dates`;
 
 DROP TABLE IF EXISTS `#__roombooking_bookings`;
+
+DROP TABLE IF EXISTS `#__roombooking_rooms`;
+
+DROP TABLE IF EXISTS `#__roombooking_mail_templates`;
+
+DROP TABLE IF EXISTS `#__roombooking_tokens`;
